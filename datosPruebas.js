4fbb6bc8b215e1b6086abc9199225bb6a40d0120
Mario Chico <chico.developer@hotmail.com>
2023-11-07 18:48:52 -0600

    Cambios en la interfaz de pruebas. Boton para seleccionar pruebas fijado a la derecha. Se eliminaron los botones que estaban en cada nivel. El modal ahora contiene una lista para escoger el nivel de la prueba

--- datosPruebas.js
+++ datosPruebas.js
@@ -1,4 +1,9 @@
 const dateValue = document.getElementById('myDate');
+const btnPagoTransferencia = document.querySelector(".transferencia");
+const btnPagoEfectivo = document.querySelector(".efectivo");
+let pagoSeleccionado;
+console.log(btnPagoEfectivo);
+console.log(btnPagoTransferencia);
 console.log(dateValue.value);
 async function datosPrueba() {
   if(dateValue.value != ""){
@@ -6,3 +11,12 @@ async function datosPrueba() {
     console.log(datoFecha);
   }
 }
+
+document.querySelector('form')
+  .addEventListener('submit', e => {
+    e.preventDefault(); // prevent form from submitting normally
+    const data = Object.fromEntries(
+      new FormData(e.target)
+    )
+    alert(JSON.stringify(data))
+  })
