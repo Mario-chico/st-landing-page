7de11753698730617d790777f88255487bd6b476
Mario Chico <chico.developer@hotmail.com>
2023-11-08 11:06:09 -0600

    Cambio en la logica de mostrar mensaje de exito en datos enviados. Se agrego boton para cerrar el modal si no se ingresa datos. Cambios en la estructura del formulario

--- datosPruebas.js
+++ datosPruebas.js
@@ -12,11 +12,47 @@ async function datosPrueba() {
   }
 }
 
-document.querySelector('form')
-  .addEventListener('submit', e => {
-    e.preventDefault(); // prevent form from submitting normally
-    const data = Object.fromEntries(
-      new FormData(e.target)
-    )
-    alert(JSON.stringify(data))
-  })
+// document.querySelector('form')
+//   .addEventListener('submit', e => {
+//     e.preventDefault(); // prevent form from submitting normally
+//     const data = Object.fromEntries(
+//       new FormData(e.target)
+//     )
+//     alert(JSON.stringify(data))
+//   })
+
+  // function datosSeleccionados() {
+  //   document.querySelector('form').addEventListener('submit', e => {
+  //     e.preventDefault(); // prevenir el envío del formulario de forma predeterminada
+  //     const data = Object.fromEntries(new FormData(e.target));
+  //     const dataString = JSON.stringify(data);
+      
+  //     // Muestra un cuadro de diálogo de confirmación
+  //     const userConfirmed = window.confirm(dataString);
+  
+  //     if (userConfirmed) {
+  //       document.getElementById('dialog').close();
+  //     }
+  //   });
+  // }
+  function datosSeleccionados() {
+    const form = document.querySelector('form');
+    const data = new FormData(form);
+  
+    // Validación de datos
+    if (data != null) {
+      // Supongamos que los datos son válidos
+  
+      // Envío de datos al servidor (esto requiere un servidor en el lado del servidor)
+      // Aquí puedes usar AJAX (por ejemplo, Fetch API) para enviar los datos al servidor
+  
+      // Mostrar un mensaje de éxito
+      alert('Datos enviados con éxito.');
+      document.getElementById('dialog').close();
+      // Limpiar los campos del formulario
+      form.reset();
+    } else {
+      alert('Por favor, complete los campos requeridos.');
+    }
+    return false;
+  }
